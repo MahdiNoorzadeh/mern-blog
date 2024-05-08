15e25fa3170ad296b2ed91c2e73cdb1860b1952d
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-08 18:08:58 +0330

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,13 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import {useState} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, SignInSuccess, SignInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading, error: errorMessage} = useSelector(state => state.user)
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value.trim() })
@@ -13,11 +15,10 @@ export default function SignIn() {
     const hadnleSubmit = async (e) => {
       e.preventDefault()
       if (!formData.email || !formData.password){
-        return setErrorMessage('لطفا تمامی فیلد ها پر کنید!')
+        return dispatch(SignInFailure('لطفا تمامی فیلد ها پر کنید!'))
       }
       try {
-        setLoading(true)
-        setErrorMessage(null)
+        dispatch(signInStart())
         const res = await fetch ('/api/auth/signin', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
@@ -25,15 +26,14 @@ export default function SignIn() {
         })
         const data = await res.json()
         if (data.success === false) {
-          return setErrorMessage(data.message)
+          dispatch(SignInFailure(data.message))
         }
-        setLoading(false)
         if (res.ok){
+          dispatch(SignInSuccess(data))
           navigate('/')
         }
       } catch (error) {
-        setErrorMessage(error.message)
-        setLoading(false)
+        dispatch(SignInFailure(error.message))
       }
     }
   return (
