4f203dea537a5ed7169906aafc27e6abfc453b67
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-10 00:37:36 +0330

    add update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,50 @@
+import { errorHandler } from '../utlis/error.js'
+import bcryptjs from 'bcryptjs'
+import User from '../models/user.model.js'
+
 export const test = (req, res) => {
     res.json({message: 'API is working!'})
+}
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.userId){
+        return next((403, 'شما اجازه آپدیت این کاربر را ندارید!'))
+    }
+    if (req.body.password) {
+
+    if (req.body.password.length < 6) {
+        return next(errorHandler(400, 'پسورد باید حداقل 6 کارکتر داشته باشد'))
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10)
+    }
+
+    if(req.body.username){
+        if (req.body.username.length < 7 || req.body.username.length > 20){
+            return next (errorHandler(400, 'نام کاربری باید حداقل 7 کارکتر و بیشتر از 20 کارکتر نباشد.'))
+        }
+        if(req.body.username.includes(' ')){
+            return next (errorHandler(400, 'نام کاربری نمیتواند شامل اسپیس باشد.'))
+        }
+        if (req.body.username !== req.body.username.toLowerCase()){
+            return next(errorHandler(400, 'نام کاربری باید شامل حروف کوچک باشد'))
+        }
+        if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
+            return next (errorHandler(400, 'نام کاربری میتواند فقط شامل اعداد و حروف باشد'))
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+                $set:{
+                    username: req.body.username,
+                    email: req.body.email,
+                    profilePicture: req.body.profilePicture,
+                    password: req.body.password,
+                },
+            }, {new: true}
+        )
+            const {password, ...rest} = updatedUser._doc
+            res.status(200).json(rest)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
