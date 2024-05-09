94c230e2bf633e6f5c44104584fc7fa556a9e0a1
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-09 10:15:45 +0330

    make the dashboard private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -47,8 +47,8 @@ export default function Header() {
                     }
                     >
                         <Dropdown.Header>
-                            <span className="blog text-sm">@{currentUser.username}</span>
-                            <span className="blog text-sm font-medium truncate">{currentUser.email}</span>
+                            <span className="block text-sm">{currentUser.username}</span>
+                            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
                         </Dropdown.Header>
                         <Link to={'/dashboard?tab=profile'}>
                             <Dropdown.Item>پروفایل</Dropdown.Item>
