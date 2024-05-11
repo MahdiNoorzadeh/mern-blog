fd3087ea77e1ebb88883002d38a278c737958f4c
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-12 01:18:17 +0330

    complete upload post image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,49 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
+import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/storage'
+import {app} from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 export default function CreatePost() {
+    const [file, setFile] = useState(null)
+    const [imageUploadProgress, setUploadImageProgress] = useState(null)
+    const [imageUploadError, setUploadImageError] = useState(null)
+    const [formData, setFormData] = useState({})
+    const handleUploadImage = async () => {
+        try {
+            if(!file){
+                setUploadImageError('لطفا یک عکس انتخاب کنید.')
+                return
+            }
+            setUploadImageError(null)
+            const storage = getStorage(app)
+            const fileName = new Date().getTime + '-' + file.name
+            const storageRef = ref(storage, fileName)
+            const uploadTask = uploadBytesResumable(storageRef, file)
+            uploadTask.on('state_changed',(snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes * 100)
+                setUploadImageProgress(progress.toFixed(0))
+            }, 
+            (error) => {
+                setUploadImageError('بارگذاری تصویر ناموفق بود، دوباره تلاش کنید.')
+                setUploadImageProgress(null)
+            }, () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setUploadImageProgress(null)
+                    setUploadImageError(null)
+                    setFormData({...formData, image: downloadURL})
+                })
+            }
+        )
+        } catch (error) {
+            setUploadImageError('بارگذاری تصویر ناموفق بود، دوباره تلاش کنید.')
+            setUploadImageProgress(null)
+            console.log(error)
+        }
+    }
   return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className="text-center text-3xl my-7 font-semibold">
@@ -19,11 +60,22 @@ export default function CreatePost() {
                 </Select>
             </div>
             <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-                <FileInput type='file' accept="image/*"/>
-                <Button type="button" gradientDuoTone="purpleToBlue" size='sm' outline>
-                    بارگذاری عکس 
+                <FileInput type='file' accept="image/*" onChange={(e)=>setFile(e.target.files[0])}/>
+                <Button type="button" gradientDuoTone="purpleToBlue" size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress}>
+                     {
+                        imageUploadProgress ? (
+                        <div className="w-16 h-16">
+                            <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}/>
+                            
+                        </div>
+                        ) : (
+                            'بارگذاری عکس '
+                        )
+                     } 
                 </Button>
             </div>
+            {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+            {formData.image &&(<img src={formData.image} alt="Cover Image" className="w-full h-72 object-cover"/>)}
             <ReactQuill theme="snow" placeholder="نوشتن را شروع کنید" className="h-72 mb-12" required/>
             <Button type="submit" gradientDuoTone='greenToBlue' outline>
                 انتشار
