ab372b3f870d7e5deecd1b2312472569ed37f280
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-12 01:43:57 +0330

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/st
 import {app} from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
     const [file, setFile] = useState(null)
     const [imageUploadProgress, setUploadImageProgress] = useState(null)
     const [imageUploadError, setUploadImageError] = useState(null)
     const [formData, setFormData] = useState({})
+    const [publishError, setPublishError] = useState()
+    const navigate = useNavigate()
     const handleUploadImage = async () => {
         try {
             if(!file){
@@ -44,15 +47,42 @@ export default function CreatePost() {
             console.log(error)
         }
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await fetch ('/api/post/creat', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                }, 
+                body: JSON.stringify(formData)
+            })
+            const data = await res.json()
+            if(!res.ok){
+                setPublishError(data.message)
+                return
+            } 
+            if (res.ok) {
+                setPublishError(null)
+                navigate(`/post/${data.slug}`)
+            }
+        } catch (error) {
+            setPublishError('مشکلی پیش آمده، دوباره تلاش کنید.')
+        }
+    }
+
   return (
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className="text-center text-3xl my-7 font-semibold">
             نشر خبر 
         </h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                <TextInput type="text" placeholder="عنوان" required id='title' className="flex-1"/>
-                <Select>
+                <TextInput type="text" placeholder="عنوان" required id='title' className="flex-1" onChange={(e) => setFormData({...formData, title: e.target.value})}/>
+                <Select
+                    onChange={(e) => ({...formData, category: e.target.value})}
+                >
                     <option value='uncategorized'>یک دسته بندی انتخاب کنید</option>
                     <option value='programming'>برنامه نویسی</option>
                     <option value='gameDev'>بازی سازی</option>
@@ -76,10 +106,15 @@ export default function CreatePost() {
             </div>
             {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
             {formData.image &&(<img src={formData.image} alt="Cover Image" className="w-full h-72 object-cover"/>)}
-            <ReactQuill theme="snow" placeholder="نوشتن را شروع کنید" className="h-72 mb-12" required/>
+            <ReactQuill theme="snow" placeholder="نوشتن را شروع کنید" className="h-72 mb-12" required onChange={(value) => {
+                setFormData({...formData, content: value})
+            }}/>
             <Button type="submit" gradientDuoTone='greenToBlue' outline>
                 انتشار
             </Button>
+            {
+                publishError && <Alert className="mt-5" color='failure'>{publishError}</Alert>
+            }
         </form>
         </div>
   )
