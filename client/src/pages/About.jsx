109019f03093b500a137a664ee62fc5a90544da3
Mahdi <mahdinoorzadeh1@gmail.com>
2024-05-20 11:40:27 +0330

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,22 @@
 export default function About() {
   return (
-    <div>
-      About
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">درباره سایت خبری مهدی</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+          <p>
+              پروژه پایانی دانشگاه من با عنوان "سایت خبری مهدی" با استفاده از تکنولوژی 
+              <span className="english-text"> MERN (MongoDB, Express, React, Node.js) </span>
+              طراحی و پیاده‌سازی شده است. هدف اصلی این پروژه ایجاد یک پلتفرم خبری مدرن و کارآمد است که بتواند نیازهای مقالات شخصی و خبرگزاری‌های کوچک و در حال رشد را برآورده سازد.
+            </p>
+
+            <p>
+              این پروژه به صورت آزاد در گیت هاب قابل دسترسی میباشد و میتوانید بهبود های مدنظرتون رو اونجا مطرح کنید 
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,11 @@
+import CallToAction from '../components/CallToAction'
+
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>همکاری با ما در تبلیغات</h1>
+      <p className='text-md text-gray-500'>فرصت استسنائی برای همکاری گسترده در تبلیغات اینترنتی با میزان بازخورد بالا رو از دست ندید!</p>
+      <CallToAction/>
+    </div>
   )
 }
